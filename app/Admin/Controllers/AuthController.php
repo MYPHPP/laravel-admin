51be0f8cf77861f96bb1bf32aeb53682ee9cbbf4
fengbo <ycpfb>
2019-12-06 18:07:51 +0800

    laravel admin

---
+++ app/Admin/Controllers/AuthController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Admin\Controllers;
+
+use Encore\Admin\Controllers\AuthController as BaseAuthController;
+
+class AuthController extends BaseAuthController
+{
+    public function __construct()
+    {
+        config([
+            'admin.name' => trans('admin.website_login_name'),
+            'admin.logo' => trans('admin.website_left_tip'),
+            'admin.logo-mini' => trans('admin.website_left_tip_mimi'),
+        ]);
+    }
+}
